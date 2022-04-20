c22b58df59770b3f483efc37f029389895a97294
Filip <vasiljevic.filip@gmail.com>
2022-04-20 22:48:26 +0200

    dodatak koda i konfiguracije za bazu

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAlruCO68db8KaNbIaDa8WnE7IOCBSCBzQ",
+  authDomain: "evidencijaservisnihnaloga.firebaseapp.com",
+  databaseURL:
+    "https://evidencijaservisnihnaloga-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "evidencijaservisnihnaloga",
+  storageBucket: "evidencijaservisnihnaloga.appspot.com",
+  messagingSenderId: "1012416515760",
+  appId: "1:1012416515760:web:2ceee44ea3ff94b197b575",
+};
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,100 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <h3 text-center>Za login odaberite vaše ime</h3>
+  <div class="q-pa-md column q-gutter-sm">
+    <q-btn color="primary" label="Dean Ladavec" @click="dean = true" />
+    <q-btn color="primary" label="Elvis Jurić" @click="elvis = true" />
+    <q-btn color="primary" label="Žarko Kovačević" @click="zarko = true" />
+    <q-btn color="primary" label="Goran Močibob" @click="goran = true" />
+    <q-btn color="primary" label="Stefano Brozan" @click="stefano = true" />
+    <q-btn color="primary" label="Ervin Štifanić" @click="ervin = true" />
+    <q-btn color="primary" label="Admin" @click="admin = true" />
+  </div>
+  <q-dialog v-model="dean">
+    <q-card>
+      <q-card-section>
+        <q-input v-model="text" label="Email" />
+        <q-input v-model="text" label="Password" />
+        <q-btn flat label="Odustani" color="primary" v-close-popup />
+        <q-btn flat label="Prihvati" color="primary" v-close-popup />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+  <q-dialog v-model="elvis">
+    <q-card>
+      <q-card-section>
+        <q-input v-model="text" label="Email" />
+        <q-input v-model="text" label="Password" />
+        <q-btn flat label="Odustani" color="primary" v-close-popup />
+        <q-btn flat label="Prihvati" color="primary" v-close-popup />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+  <q-dialog v-model="zarko">
+    <q-card>
+      <q-card-section>
+        <q-input v-model="text" label="Email" />
+        <q-input v-model="text" label="Password" />
+        <q-btn flat label="Odustani" color="primary" v-close-popup />
+        <q-btn flat label="Prihvati" color="primary" v-close-popup />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+  <q-dialog v-model="goran">
+    <q-card>
+      <q-card-section>
+        <q-input v-model="text" label="Email" />
+        <q-input v-model="text" label="Password" />
+        <q-btn flat label="Odustani" color="primary" v-close-popup />
+        <q-btn flat label="Prihvati" color="primary" v-close-popup />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+  <q-dialog v-model="stefano">
+    <q-card>
+      <q-card-section>
+        <q-input v-model="text" label="Email" />
+        <q-input v-model="text" label="Password" />
+        <q-btn flat label="Odustani" color="primary" v-close-popup />
+        <q-btn flat label="Prihvati" color="primary" v-close-popup />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+  <q-dialog v-model="ervin">
+    <q-card>
+      <q-card-section>
+        <q-input v-model="text" label="Email" />
+        <q-input v-model="text" label="Password" />
+        <q-btn flat label="Odustani" color="primary" v-close-popup />
+        <q-btn flat label="Prihvati" color="primary" v-close-popup />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+  <q-dialog v-model="admin">
+    <q-card>
+      <q-card-section>
+        <q-input v-model="text" label="Email" />
+        <q-input v-model="text" label="Password" />
+        <q-btn flat label="Odustani" color="primary" v-close-popup />
+        <q-btn flat label="Prihvati" color="primary" v-close-popup />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { ref } from "vue";
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+export default {
+  setup() {
+    return {
+      dean: ref(false),
+      elvis: ref(false),
+      zarko: ref(false),
+      goran: ref(false),
+      stefano: ref(false),
+      ervin: ref(false),
+      admin: ref(false),
+    };
+  },
+};
 </script>
